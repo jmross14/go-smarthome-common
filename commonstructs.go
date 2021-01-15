ad737eb4db6bc91d9faef7a1edaf901e38f765f4
Jonathon Ross <jonathon.m.ross1@gmail.com>
2021-01-15 06:20:54 -0500

    Added message to trigger relay.

--- commonstructs.go
+++ commonstructs.go
@@ -7,6 +7,21 @@ type TemperatureReading struct {
 	Temperature float64 `json:"temperature"`
 }
 
+// Status is used to signify a status
+type Status int
+
+const (
+	// Off signifies the Off status
+	Off Status = iota
+	// On signifies the On status
+	On Status = iota
+)
+
+// TriggerRelay is used to trigger a relay on/off
+type TriggerRelay struct {
+	Status Status
+}
+
 // SensorType is meant to distinguish the type of sensor.
 type SensorType int
 
